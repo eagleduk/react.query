c4c64f3e54f05bdd2fd1cd6d622c5d1912b79708
SJ.Lee <eagleduk@gmail.com>
2023-05-18 00:01:37 +0900

    프로젝트 기본 설정 및 useQuery 구현
    프로젝트 시작 설정
    기본 useQuery 함수 구현

--- base-lazy-days/client/src/components/staff/utils.ts
+++ base-lazy-days/client/src/components/staff/utils.ts
@@ -4,6 +4,7 @@ export function filterByTreatment(
   staff: Staff[],
   treatmentName: string,
 ): Staff[] {
+  if (treatmentName === 'all') return staff;
   return staff.filter((person) =>
     person.treatmentNames
       .map((t) => t.toLowerCase())
